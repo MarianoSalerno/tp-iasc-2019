303ecd0eaad71eb1f6dc401cedf1ea1f174a4191
Mariano Salerno <msalerno@elementum.com>
2019-06-25 21:22:45 -0300

    implementado limite de tamaño para claves/valores

--- cliente/cliente.js
+++ cliente/cliente.js
@@ -3,18 +3,29 @@ const app = express()
 const port = 3000
 const map = {}
 const bodyParser = require('body-parser')
+const maxSize = 10
 
 app.use( bodyParser.json() );
 
 app.get('/get/:key', (req, res, next) => {
-	const value = map[req.params.key]
+	const key = req.params.key
+	const value = map[key]
+
+	if (tamañoSuperaMaximo(key) || tamañoSuperaMaximo(value)) return next(new Error('key o value superan tamaño maximo'))
 	if (value === undefined) return next(new Error('key not found'))
+
 	res.send(value)
 })
 
 app.post('/put/:key', (req, res, next) => {
-	map[req.params.key] = req.body.value
+	const key = req.params.key
+	const value = req.body.value
+
+	if (tamañoSuperaMaximo(key) || tamañoSuperaMaximo(value)) return next(new Error('key o value superan tamaño maximo'))
+	map[key] = value
 	res.send("ok")
 })
 
 app.listen(port, () => console.log(`Cliente levantado en el puerto: ${port}!`))
+
+const tamañoSuperaMaximo = (keyOValue) => keyOValue.length > maxSize
