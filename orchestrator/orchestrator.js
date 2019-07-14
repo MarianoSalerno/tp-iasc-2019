ce78434d8b4963916e7caab0ffc5966e9fc63522
Unknown <occhiuzzi.santiago@gmail.com>
2019-07-13 21:46:10 -0300

    Explicación de cómo suscribirse

--- orchestrator/orchestrator.js
+++ orchestrator/orchestrator.js
@@ -12,19 +12,6 @@ function changeMasterOrchestrator() {
 } 
 
 app.use( bodyParser.json() ); 
- 
-app.post('/get-conf/:node', (req, res, next) => { 
-	if(cluster.thisIsSlave) { 
-		res.status(400).send('Not the master orchestrator'); 
-	} else {
-		const node = req.params.node // TODO: Ver si hace falta diferenciar la info según quién pregunte
-		res.json(cluster.snapshot()) 
-	}
-}) 
- 
-app.post('/change-to-master',() => { 
-	isMaster = true 
-}) 
 
 app.get('/healthcheck',(req, res, next) => { 
 	// Hoy devuelve 200, pero los orquestadores podrían tener lógica diferente de los nodos de datos para verificar que están vivos y funcionan bien
@@ -35,12 +22,9 @@ const validNodeTypes = new Set(["orchestrator", "data", "client"])
 app.post('/subscribers/:type', (req, res, next) => { 
 	const type = req.params.type
 	if (!validNodeTypes.has(type)){
-		console.log(validNodeTypes)
-		console.log(type)
-		console.log(validNodeTypes.has(type))
 		let message = `What is a ${type}? I don't know that kind of node!`
 		console.log(message)
-		res.status(400).json({error: "I don't know that kind of node!"})
+		res.status(400).json({error: message})
 	} else {
 		const subscriberPort = req.body.port
 		subscriptions.publisher.addSubscriber(subscriberPort)
--- orchestrator/subscriptions.js
+++ orchestrator/subscriptions.js
@@ -25,11 +25,12 @@ function acceptNews(app, whatToDoWhithNewShapshot){
 	}) 
 }
 
-function subscribeAs(nodeType, myPort, targetPort){
+function subscribeAs(nodeType, myPort, targetPort, whatToDoWhithNewShapshot){
 	axios.post(`http://localhost:${targetPort}/subscribers/${nodeType}`, {port: myPort}, {timeout: 1000})
 		.then((response) => {
 			console.log(`I'm subscribed to ${targetPort} as ${nodeType}!`)
-			console.log("New Snapshot:", response.data) // TODO: Hacer algo con el snapshot recibido. Podemos guardarlo y ya, o bien, tener un callback y hacer alguna cosa más loca
+			console.log("New Snapshot:", response.data)
+			whatToDoWhithNewShapshot(response.data)
 		})
 		.catch((error) => console.log(`Something failed subscribing to ${targetPort}. More info: ${error}`))
 }
@@ -37,15 +38,40 @@ function subscribeAs(nodeType, myPort, targetPort){
 function partialSubscription(nodeType){
 	return (app, myPort, targetPort, whatToDoWhithNewShapshot) => {
 		acceptNews(app, whatToDoWhithNewShapshot)
-		subscribeAs(nodeType, myPort, targetPort)
+		subscribeAs(nodeType, myPort, targetPort, whatToDoWhithNewShapshot)
 	}
 }
 
+// El objeto "publisher" solo sirve para los orquestadores por ahora.
+// Más adelante podría servir para que podamos recibir novedades de varios nodos de distinto tipo, todo cruzado, pero por ahora solo queremos que el orquestador mande novedades.
 exports.publisher = {
 	addSubscriber: addSubscriber,
 	notifyNewsToAllSubscribers: notifyNewsToAllSubscribers
 }
 
+/*
+	Todos los nodos tienen que usar el objeto suscriber. Todos deben suscribirse al orquestador activo ni bien se inician.
+
+	Primero hay que importar el paquete:
+	const suscriptions = require('suscriptions');
+
+	Después, necesitamos tener declarada alguna función para saber qué hacer cuando recibimos un snapshot actual del cluster:
+	function whatToDoWithSnapshot(snapshot) { 
+		código mágico para reaccionar con cada snapshot del cluster que nos llega
+	}
+
+	Luego, tenemos que poder recibir novedades, y reaccionar ante ellas. Así que tenemos que mandar:
+	- app de express, para que nos cree la ruta de /news idéntica en cada nodo que quiera tener esta funcionalidad
+	- nuestro puerto
+	- el puerto del orquestador activo (master) en este momento
+	- la función mágica creada más arriba
+
+	Entonces, por ejemplo, un cliente comienza a aceptar novedades, y se suscribe al master, con esta línea:
+	suscriptions.subscribeAsClient(app, 5000, 8000, whatToDoWithSnapshot)
+
+	Y un nodo de datos, así:
+	suscriptions.subscribeAsDataNode(app, 5000, 8000, whatToDoWithSnapshot)
+*/
 exports.subscriber = {
 	subscribeAsOrchestrator: partialSubscription("orchestrator"),
 	subscribeAsDataNode: partialSubscription("dataNode"),
