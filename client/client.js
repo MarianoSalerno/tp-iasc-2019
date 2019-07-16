41d0acb944477ba61fec2caf4a111ba7d1c31001
Sofía Cortés <sofiavcortes@gmail.com>
2019-07-16 09:14:14 -0300

    Fix upsert client. Remove unused methods in client

--- client/client.js
+++ client/client.js
@@ -6,21 +6,7 @@ const hash = require('./hash-function.js')
 const axios = require('axios');
 const app = express()
 var dataNodes
-var replacementOrchestrators
 var totalPartitions
-
-function findMasterOrchestrator() {
-	for (let ip of replacementOrchestrators) { //todo: arreglar
-		axios.post(`${ip}/subscribers/client`)
-			.then((response) => {
-				console.log(`Registered client node to ${ip}!`)
-				dataNodes = response.data.shards
-				replacementOrchestrators = response.data.orchestrators.replacementsInOrder
-				totalPartitions = response.data.totalPartitions
-			})
-			.catch((error) => console.log(`Error: Orchestrator ${ip} response: ${error}`))
-	}
-}
 
 function getDatanodePortByPartition(partition, res, key) {
 	const dataNode = dataNodes.filter((x) => x.partitions.from <= partition && partition <= x.partitions.to)[0]
@@ -44,85 +30,81 @@ function getDatanodesInformation(snapshot) {
 
 app.use(bodyParser.json());
 
-app.get('/get/:key', (req, res, next) => {
-	try {
-		const key = req.params.key
-		partition = hash.getPartitionFromKey(totalPartitions, key)
-		const nodePort = getDatanodePortByPartition(partition, res, key)
-		if (nodePort === "error") {
-			return
-		}
-
-		console.log(`http://localhost:${nodePort}/keys/${partition}/${key}`)
-		axios.get(`http://localhost:${nodePort}/keys/${partition}/${key}`)
-			.then((response) => {
-				console.log(`Query result: ${JSON.stringify(response.data)}`)
-				res.json(response.data)
-			})
-			.catch((error) => {
-				console.log(`ERROR: ${JSON.stringify(error.response.data)}`)
-				res.status(400).json(error.response.data)
-			})
-	} catch (e) {
-		findMasterOrchestrator()
-		res.status(404).json({ "error": "No configuration found; trying to reconnect master orchestrator. Try again in a few seconds" })
+app.get('/data/:key', (req, res, next) => {
+	const key = req.params.key
+	partition = hash.getPartitionFromKey(totalPartitions, key)
+	const nodePort = getDatanodePortByPartition(partition, res, key)
+	if (nodePort === "error") {
+		return
 	}
+
+	console.log(`http://localhost:${nodePort}/keys/${partition}/${key}`)
+	axios.get(`http://localhost:${nodePort}/keys/${partition}/${key}`)
+		.then((response) => {
+			console.log(`Query result: ${JSON.stringify(response.data)}`)
+			res.json(response.data)
+		})
+		.catch((error) => {
+			console.log(`ERROR: ${JSON.stringify(error.response.data)}`)
+			res.status(400).json(error.response.data)
+		})
 })
 
 const upsert = (req, res, next) => {
-	try {
-		const key = req.params.key
-		const value = req.params.value
-		partition = hash.getPartitionFromKey(totalPartitions, key)
-		const nodePort = getDatanodePortByPartition(partition, res, key)
-		if (nodePort === "error") {
-			return
-		}
+	const key = req.body.key
+	const value = req.body.value
 
-		axios.post(`http://localhost:${nodePort}/keys/${partition}`,
-			{
-				"key": key,
-				"value": value
-			}
-		)
-			.then((response) => {
-				console.log(`Insertion: ${JSON.stringify(response.data)}`)
-				res.json(response.data)
-			})
-			.catch((error) => {
-				console.log(`ERROR: ${JSON.stringify(error.response.data)}`)
-				res.status(400).json(error.response.data)
-			})
-	} catch (e) {
-		findMasterOrchestrator()
-		res.status(404).json({ "error": "No configuration found; trying to reconnect master orchestrator. Try again in a few seconds" })
+	if (key === undefined) {
+        res.status(400).json({ "response": "Key is missing" })
+        return
+    }
+
+    if (value === undefined) {
+        res.status(400).json({ "response": "Value is missing" })
+        return
 	}
+	
+	partition = hash.getPartitionFromKey(totalPartitions, key)
+	const nodePort = getDatanodePortByPartition(partition, res, key)
+	if (nodePort === "error") {
+		return
+	}
+
+	axios.post(`http://localhost:${nodePort}/keys/${partition}`,
+		{
+			"key": key,
+			"value": value
+		}
+	)
+		.then((response) => {
+			console.log(`Insertion. Data: ${key}-${value}. ${JSON.stringify(response.data)}`)
+			res.json(response.data)
+		})
+		.catch((error) => {
+			console.log(`ERROR: ${JSON.stringify(error.response.data)}`)
+			res.status(400).json(error.response.data)
+		})
 }
 
-app.get('/insert/:key/:value', upsert)
+app.post('/data', upsert)
 
 app.get('/delete/:key', (req, res, next) => {
-	try {
-		const key = req.params.key
-		partition = hash.getPartitionFromKey(totalPartitions, key)
-		const nodePort = getDatanodePortByPartition(partition, res, key)
-		if (nodePort === "error") {
-			return
-		}
-
-		axios.delete(`http://localhost:${nodePort}/keys/${partition}/${key}`)
-			.then((response) => {
-				console.log(`Deletion: ${JSON.stringify(response.data)}`)
-				res.json(response.data)
-			})
-			.catch((error) => {
-				console.log(`ERROR: ${JSON.stringify(error.response.data)}`)
-				res.status(400).json(error.response.data)
-			})
-	} catch (e) {
-		console.log(`No configuration found; trying to reconnect master orchestrator`)
-		findMasterOrchestrator()
+	const key = req.params.key
+	partition = hash.getPartitionFromKey(totalPartitions, key)
+	const nodePort = getDatanodePortByPartition(partition, res, key)
+	if (nodePort === "error") {
+		return
 	}
+
+	axios.delete(`http://localhost:${nodePort}/keys/${partition}/${key}`)
+		.then((response) => {
+			console.log(`Deletion: ${JSON.stringify(response.data)}`)
+			res.json(response.data)
+		})
+		.catch((error) => {
+			console.log(`ERROR: ${JSON.stringify(error.response.data)}`)
+			res.status(400).json(error.response.data)
+		})
 })
 
 app.get('/healthcheck', (req, res, next) => {
